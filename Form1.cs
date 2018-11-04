9bc2468c97aea7ddd7080e58fb125b56626d089b
성수 박 <mysql_connect@naver.com>
2018-11-05 08:38:27 +0900

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SnakeGame
+{
+    public partial class Form1 : Form
+    {
+        private Snake snake = new Snake();
+        Size snake_size;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.Width = 800;
+            this.Height = 600;
+            snake.Init_Snake();
+            snake_size = snake.snake_size;
+            Thread thread = new Thread(()=> {
+                while (true)
+                {
+                    this.Invalidate();
+                    Thread.Sleep(50);
+
+                }
+            });
+            thread.Start();
+            
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(Brushes.White, new Rectangle(0,0,800,600));
+            foreach(Point position in snake.snake_body)
+            {
+                e.Graphics.FillRectangle(Brushes.Black,new Rectangle(position.X, position.Y, snake_size.Width, snake_size.Height));
+            }
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Up)
+            {
+                snake.Move_Snake(0);
+                return;
+            }
+            else if(e.KeyCode == Keys.Down)
+            {
+                snake.Move_Snake(1);
+
+                return;
+            }
+            else if(e.KeyCode == Keys.Left)
+            {
+                snake.Move_Snake(2);
+
+                return;
+            }
+            else if(e.KeyCode == Keys.Right)
+            {
+                snake.Move_Snake(3);
+
+
+                return;
+            }
+        }
+    }
+}
---
+++ Snake.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using static System.Runtime.CompilerServices.RuntimeHelpers;
+
+namespace SnakeGame
+{
+    public class Snake
+    {
+        
+        public Point position = new Point(0,0);
+       
+        public List<Point> snake_body = new List<Point>();
+        public Size snake_size = new Size(100,30);
+
+        public void Init_Snake()
+        {
+            
+            for(int i = 0; i < 3; i++)
+            {
+                position = new Point(position.X+15,position.Y);
+                snake_body.Add(position);
+                
+            }
+        }
+        public void Move_Snake(int status_key)
+        {
+            if (status_key == 0)
+            {
+                
+            }
+            else if (status_key == 1)
+            {
+
+            }
+            else if (status_key == 2)
+            {
+
+            }
+            else if (status_key == 3)
+            {
+
+            }
+
+
+        }
+
+    }
+}
